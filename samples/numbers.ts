532b8472754fe85d5dc555d51557ef1838b7dd25
Paul Shannon <a@devpaul.com>
2018-02-08 10:09:30 -0700

    Added node_modules for publish

--- samples/numbers.ts
+++
@@ -1,3 +0,0 @@
-const cost = 1_000_000;
-const hex = 0xDE_AD_BE_EF;
-const small = 0.000_000_1;
--- samples/optional-catch.ts
+++
@@ -1,8 +0,0 @@
-export function optional_catch() {
-	try {
-		throw new Error();
-	}
-	catch {
-		// Don't care about the error
-	}
-}
